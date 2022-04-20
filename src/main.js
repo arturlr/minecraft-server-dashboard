2ea214438be2e8b397338ea0d2974f7fa6080f38
Rodrigues <artrodri@a483e77799ff.ant.amazon.com>
2022-04-19 23:40:07 -0700

    redirect oauth

--- src/main.js
+++ src/main.js
@@ -7,36 +7,13 @@ import Amplify, * as AmplifyModules from 'aws-amplify'
 import { AmplifyPlugin } from 'aws-amplify-vue'
 import aws_exports from './aws-exports'
 
-var urlsIn = aws_exports.oauth.redirectSignIn.split(",");
-var urlsOut = aws_exports.oauth.redirectSignOut.split(",");
 const oauth = {
   domain: aws_exports.oauth.domain,
   scope: aws_exports.oauth.scope,
-  redirectSignIn: aws_exports.oauth.redirectSignIn,
-  redirectSignOut: aws_exports.oauth.redirectSignOut,
+  redirectSignIn: `${window.location.origin}/`,
+  redirectSignOut: `${window.location.origin}/`,
   responseType: aws_exports.oauth.responseType
 };
-
-var hasLocalhost  = (hostname) => Boolean(hostname.match(/localhost/) || hostname.match(/127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}/));
-var locationArr = location.hostname.split(".")
-let authName = ""
-if (locationArr.length >=3 ) {
-   authName = 'auth.'
-   for (let i = 0; i < locationArr.length; i++) { 
-      authName += locationArr[i] + '.'
-   }
-   authName = authName.slice(0, -1); 
-}
-
-if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
-  urlsIn.forEach((e) =>   { if (hasLocalhost(e)) { oauth.redirectSignIn = e; }});
-  urlsOut.forEach((e) =>  { if (hasLocalhost(e)) { oauth.redirectSignOut = e; }});
-}
-else {
-  urlsIn.forEach((e) =>   { if (!hasLocalhost(e)) { oauth.redirectSignIn = e; }});
-  urlsOut.forEach((e) =>  { if (!hasLocalhost(e)) { oauth.redirectSignOut = e; }});
-  oauth.domain = authName
-}
 
 var configUpdate = aws_exports;
 configUpdate.oauth = oauth;
@@ -44,9 +21,6 @@ configUpdate.oauth = oauth;
 Amplify.configure(configUpdate);
 
 Vue.use(AmplifyPlugin, AmplifyModules)
-
-Vue.config.productionTip = false
-
 Vue.config.productionTip = false
 
 new Vue({
