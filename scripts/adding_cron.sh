43ad9ad315c9c84f7d6ad9b0d5752b5404c3250f
Rodrigues <artrodri@a483e77799ff.ant.amazon.com>
2022-06-09 19:46:18 -0700

    adding workingdir to the script

--- scripts/adding_cron.sh
+++ scripts/adding_cron.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-cat << EOF > ${HOME}/port_count.sh
+WORKING_DIR="/opt/aws"
+
+cat << EOF > ${WORKING_DIR}/port_count.sh
 #!/bin/bash
-INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-PORT_COUNT=$(netstat -an | grep ESTABLISHED | grep '\.25565' | wc -l)
+INSTANCE_ID=\$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+PORT_COUNT=\$(netstat -an | grep ESTABLISHED | grep '\.25565' | wc -l)
 aws cloudwatch put-metric-data --metric-name UserCount --dimensions Instance=\${INSTANCE_ID} --namespace "MinecraftDashboard" --value \${PORT_COUNT}
 EOF
 
-chmod +x ${HOME}/port_count.sh
+chmod +x ${WORKING_DIR}/port_count.sh
 
 sudo crontab -l > cron_bkp
-sudo echo "*/3 * * * * sudo ${HOME}/port_count.sh >/dev/null 2>&1" >> cron_bkp
+sudo echo "*/3 * * * * sudo ${WORKING_DIR}/port_count.sh >/dev/null 2>&1" >> cron_bkp
 sudo crontab cron_bkp
 sudo rm cron_bkp
