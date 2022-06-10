03a70623b49452fae14854bbddee78554589a13c
Rodrigues <artrodri@a483e77799ff.ant.amazon.com>
2022-06-09 19:54:51 -0700

    adding region to the script

--- scripts/adding_cron.sh
+++ scripts/adding_cron.sh
@@ -6,7 +6,8 @@ cat << EOF > ${WORKING_DIR}/port_count.sh
 #!/bin/bash
 INSTANCE_ID=\$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 PORT_COUNT=\$(netstat -an | grep ESTABLISHED | grep '\.25565' | wc -l)
-aws cloudwatch put-metric-data --metric-name UserCount --dimensions Instance=\${INSTANCE_ID} --namespace "MinecraftDashboard" --value \${PORT_COUNT}
+REGION="\$(curl -s http://169.254.169.254/latest/meta-data/local-hostname | cut -d . -f 2)"
+aws cloudwatch put-metric-data --metric-name UserCount --dimensions Instance=\${INSTANCE_ID} --namespace "MinecraftDashboard" --value \${PORT_COUNT} --region \${REGION}
 EOF
 
 chmod +x ${WORKING_DIR}/port_count.sh
