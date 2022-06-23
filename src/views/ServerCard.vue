8cc1f10fa8d73210447747c2e5b03fb8b38e50ff
Artur Rodrigues <arturlr@live.com>
2022-06-23 11:46:37 -0700

    fixing json inputs

--- src/views/ServerCard.vue
+++ src/views/ServerCard.vue
@@ -296,7 +296,7 @@
               outlined
               small
               class="pa-2"
-              @click="triggerAction('start',{ 'instanceId':  serverId }, true)"
+              @click="triggerAction('start',{ 'instanceId': '\'' + serverId + '\'' }, true)"
             >
               Start
             </v-btn>
@@ -529,7 +529,7 @@ export default {
       this.settingsDialogLoading = true;
       
       if (submit) {
-        await this.triggerAction("setintanceinfo", {"rc":this.runCommand,"wd":this.workingDir,"am":this.alarmMetric,"at":this.alarmThreshold,'instanceId': this.serverId })          
+        await this.triggerAction("setintanceinfo", {"rc":'"' + this.runCommand + '"',"wd":'"' + this.workingDir + '"',"am":'"' + this.alarmMetric + '"',"at":'"' + this.alarmThreshold + '"',"instanceId": '"' + this.serverId + '"' })          
         this.settingsDialog = false;
       } else {
         // Default values
@@ -538,7 +538,7 @@ export default {
         this.alarmMetric = "";
         this.alarmThreshold = "";
 
-        const resp = await this.triggerAction("getintanceinfo", {'instanceId': this.serverId});
+        const resp = await this.triggerAction("getintanceinfo", {"instanceId": '"' + this.serverId + '"'});
         console.log(resp)
       }
 
