5e87ecb16768c1054f9f0e87d35b28280e3dfbba
Artur Rodrigues <artrodri@amazon.com>
2025-11-26 23:43:20 -0800

    fixing cloudwatch metrics collection for networking
    Co-authored-by: Artur Rodrigues <11309240+arturlr@users.noreply.github.com>

--- lambdas/getServerMetrics/index.py
+++ lambdas/getServerMetrics/index.py
@@ -47,7 +47,10 @@ def handler(event, context):
             namespace='MinecraftDashboard',
             start_time=start_time,
             end_time=end_time,
-            statistic='Sum'
+            statistic='Sum',
+            dimensions=[
+                {'Name': 'InstanceId', 'Value': instance_id}
+            ]
         )
         
         # Fetch Memory metrics (custom metric from CloudWatch agent)
